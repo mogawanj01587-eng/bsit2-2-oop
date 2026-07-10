72ec8f0957066a67966ff08a7a462cbf07dcec8a
Juhaner Mogawan <mogawanj01587@liceo.edu.ph>
2026-07-10 11:43:20 +0800

    lab activity 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
+
+        int choice;
+
+        do {
+
+            System.out.println("\n===== LIBRARY INFORMATION SYSTEM =====");
+            System.out.println("1. Add Book");
+            System.out.println("2. List Books");
+            System.out.println("3. Borrow Book");
+            System.out.println("4. Return Book");
+            System.out.println("5. Search Book");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+
+                    System.out.print("Enter book title: ");
+                    String title = scanner.nextLine();
+
+                    System.out.print("Enter author: ");
+                    String author = scanner.nextLine();
+
+                    Book book = new Book(title, author);
+                    library.addBook(book);
+
+                    break;
+
+                case 2:
+
+                    library.listBooks();
+
+                    break;
+
+                case 3:
+
+                    System.out.print("Enter title to borrow: ");
+                    title = scanner.nextLine();
+
+                    library.borrowBook(title);
+
+                    break;
+
+                case 4:
+
+                    System.out.print("Enter title to return: ");
+                    title = scanner.nextLine();
+
+                    library.returnBook(title);
+
+                    break;
+
+                case 5:
+
+                    System.out.print("Enter title to search: ");
+                    title = scanner.nextLine();
+
+                    library.searchBook(title);
+
+                    break;
+
+                case 0:
+
+                    System.out.println("Thank you for using the Library Information System!");
+
+                    break;
+
+                default:
+
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+        } while (choice != 0);
+
+        scanner.close();
     }
 }
